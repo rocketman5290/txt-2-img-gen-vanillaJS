e9fa577a016e8bc060cd395710bdc184341dfb4e
WILLIAM GEORGE <ROCKETMAN5290@GMAIL.com>
2023-04-04 18:01:42 -0400

    Add error handling for request

--- server.js
+++ server.js
@@ -17,16 +17,22 @@ app.use(cors());
 app.use(express.json());
 
 app.post('/dream', async (req, res) => {
-    const prompt = req.body.prompt;
+    try {
+        const prompt = req.body.prompt;
 
-    const aiResponse = await openai.createImage({
-        prompt,
-        n: 1,
-        size: '1024x1024',
-    });
+        const aiResponse = await openai.createImage({
+            prompt,
+            n: 1,
+            size: '1024x1024',
+        });
 
-    const image = aiResponse.data.data[0].url;
-    res.send({ image });
+        const image = aiResponse.data.data[0].url;
+        res.send({ image });
+
+    } catch (error) {
+        console.error(500);
+        res.status(500).send(error?.response.data.error.message || "Something went wrong");
+    }
 });
 
 app.listen(8080, () => {
