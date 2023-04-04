1bb06e7942c3504b9a9c1b36d61b7616544023fe
WILLIAM GEORGE <ROCKETMAN5290@GMAIL.com>
2023-04-04 17:47:48 -0400

    Debug issue with image loading

--- main.js
+++ main.js
@@ -1,5 +1,24 @@
-import './style.css'
-import { setupCounter } from './counter.js'
+import './style.css';
 
+const form = document.querySelector('form');
 
-setupCounter(document.querySelector('#counter'))
+form.addEventListener('submit', async (e) => {
+    e.preventDefault();
+
+    const data = new FormData(form);
+
+    const response = await fetch('http://localhost:8080/dream', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        prompt: data.get('prompt'),
+      }),
+    });
+
+    const { image } = await response.json();
+
+    const result = document.querySelector('#result');
+    result.innerHTML = `<img src="${image}" width="512" />`;
+});
