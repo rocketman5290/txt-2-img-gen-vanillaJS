b03b096e9388e4617f6714bb4b8737a23ed70c96
WILLIAM GEORGE <ROCKETMAN5290@GMAIL.com>
2023-04-04 17:18:23 -0400

    Add html elements and css styling

--- main.js
+++ main.js
@@ -1,7 +1,5 @@
 import './style.css'
 import { setupCounter } from './counter.js'
 
-document.querySelector('#app').innerHTML = `<h1>Promt to Image Generator App</h1>`
-
 
 setupCounter(document.querySelector('#counter'))
