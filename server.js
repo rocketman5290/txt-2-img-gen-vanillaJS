1766b9184c3a4c1fcfc8c29ac2e137a7ba4031f7
WILLIAM GEORGE <ROCKETMAN5290@GMAIL.com>
2023-04-04 16:43:14 -0400

    Add post endpoint to send prompt to open AI for image generation

--- server.js
+++ server.js
@@ -9,3 +9,28 @@ const configuration = new Configuration({
 
 const openai = new OpenAIApi(configuration);
 
+import express from 'express';
+import cors from 'cors';
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+app.post('/dream', async (req, res) => {
+    const prompt = req.body.prompt;
+
+    const aiResponse = await openai.createImage({
+        prompt,
+        n: 1,
+        size: '1024x1024',
+    });
+
+    const image = aiResponse.data.data[0].url;
+    res.send({ image });
+});
+
+app.listen(8080, () => {
+    console.log('Server is running on port http://localhost:8080/dream');
+});
+
+//run server and client: $node server.js && npm run dev
