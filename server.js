df85d3ea2ec904392b519530086e574ca79177dc
WILLIAM GEORGE <ROCKETMAN5290@GMAIL.com>
2023-04-04 16:36:09 -0400

    set api key and initialize OPENAI SDK

---
+++ server.js
@@ -0,0 +1,11 @@
+import * as dotenv from 'dotenv';
+dotenv.config();
+
+import { Configuration, OpenAIApi } from 'openai';
+
+const configuration = new Configuration({
+    apiKey: process.env.OPENAI_API_KEY,
+});
+
+const openai = new OpenAIApi(configuration);
+
