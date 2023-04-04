451bd62f248ce2c09b10788c4166062b887e7b55
WILLIAM GEORGE <ROCKETMAN5290@GMAIL.com>
2023-04-04 18:28:06 -0400

    Add client side error handling

--- server.js
+++ server.js
@@ -38,5 +38,3 @@ app.post('/dream', async (req, res) => {
 app.listen(8080, () => {
     console.log('Server is running on port http://localhost:8080/dream');
 });
-
-//run server and client: $node server.js && npm run dev
